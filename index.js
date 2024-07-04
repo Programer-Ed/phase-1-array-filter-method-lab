3e66655e7dbc076e7ef618e3965dc6bce4a50ac1
Edwin Ng'ang'a <moyiedwin8@gmail.com>
2024-07-04 16:13:14 +0300

    adds filters

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+
+function findMatching(drivers, name) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase().indexOf(name.toLowerCase()) !== -1;
+    });
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase().indexOf(name.toLowerCase()) === 0;
+    })
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function (driver) {
+        return driver.name.toLowerCase() === name.toLowerCase();
+    });
+}
