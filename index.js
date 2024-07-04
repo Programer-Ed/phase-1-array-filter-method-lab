312ed74b1e1cf9a58f39b5cd834da69fc8b3f75c
Edwin Ng'ang'a <moyiedwin8@gmail.com>
2024-07-04 19:22:04 +0300

    adds filter

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-
 function findMatching(drivers, name) {
-    return drivers.filter(function (driver) {
-        return driver.toLowerCase().indexOf(name.toLowerCase()) !== -1;
-    });
-}
-
-function fuzzyMatch(drivers, name) {
-    return drivers.filter(function (driver) {
-        return driver.toLowerCase().indexOf(name.toLowerCase()) === 0;
-    })
-}
-
-function matchName(drivers, name) {
-    return drivers.filter(function (driver) {
-        return driver.name.toLowerCase() === name.toLowerCase();
-    });
-}
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
